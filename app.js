e30460ec8ba32462a706e04333cea10301c6c81f
hussaindadibrahimi296-ctrl <hussaindadibrahimi296@gmail.com>
2026-08-21 09:45:59 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-// EarnZood Mini App
-
 const tg = window.Telegram?.WebApp;
 
 if (tg) {
     tg.ready();
     tg.expand();
-}
 
-// فعلاً فقط برای تست
-console.log("EarnZood Mini App loaded");
+    const user = tg.initDataUnsafe?.user;
+
+    if (user) {
+        console.log("Telegram User:", user);
+
+        const header = document.querySelector("header");
+
+        if (user.first_name) {
+            const welcome = document.createElement("p");
+
+            welcome.textContent = `سلام ${user.first_name} 👋`;
+
+            welcome.style.marginTop = "10px";
+            welcome.style.color = "#ffd54a";
+            welcome.style.fontSize = "15px";
+
+            header.appendChild(welcome);
+        }
+    }
+}
